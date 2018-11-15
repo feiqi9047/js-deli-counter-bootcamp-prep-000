00f09cf67d76e46a6dcc47107b3fd1e3b556a5b6
Fei Qi <feiqi9047@gmail.com>
2018-11-15 19:11:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,15 +14,22 @@ function nowServing(line){
   }
 }
 
-function CurrenLine(numline){
-  var str = []
-  if (numline.length===0){
-    return 'There is nobody waiting to be served!'
-  } else{
-    for(var i=0; i<numline.length; i++){
-      str += (i+1) + ". " + numline[i] + ", "
-    }
-    str = str.slice(0,str.length-2)
-    return "The line is currently: " + str
-  }
+function currentLine(line){
+var outputString;
+if (line.length === 0){
+outputString = “The line is currently empty.”
+}
+else{
+outputString = “The line is currently: ”;
+for(var i = 0; i < line.length ; i++){
+if (i === 0){
+
+outputString = outputString + (i+1) + “. ” + line[i];
+}
+else{
+outputString = outputString + “, ” + (i+1) + “. ” + line[i];
+}
+}
+}
+return outputString;
 }
