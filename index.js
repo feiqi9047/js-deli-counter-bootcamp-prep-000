a90e743372fc9fbe3a09c9fc5e7199bfc7e15fdf
Fei Qi <feiqi9047@gmail.com>
2018-11-15 17:46:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(line,name){
   line.push(name);
-  return "Welcome," + name +", you are" + "line" + "in line."
+  return "Welcome," + name +", you are" + line.length + "in line."
 }
