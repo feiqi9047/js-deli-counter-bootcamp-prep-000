f615ddea8697755b8938a3916efd5259b8ef7012
Fei Qi <feiqi9047@gmail.com>
2018-11-15 18:47:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ function nowServing(line){
   }
 }
 
-function CurrenLine(katzDeliLine){
-  if (katzDeliLine.length===0){
+function CurrenLine(line){
+  if (line.length===0){
     return 'There is nobody waiting to be served!'
   } else{
     var newLine = [],i;
-    for (i=0; i<katzDeliLine.length;i++); 
-    newLine.push(' ${i+1}. ${katzDeliLine[i]}');
+    for (i=0; i<line.length;i++); 
+    newLine.push(' ${i+1}. ${line[i]}');
   }
   return 'The line is currently: ${newLine}'
 }
