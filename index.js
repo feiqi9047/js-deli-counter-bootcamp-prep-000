41e3962103591887c780f2fd0863208a5c15ef70
Fei Qi <feiqi9047@gmail.com>
2018-11-15 18:43:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function CurrenLine(katzDeliLine){
     return 'There is nobody waiting to be served!'
   } else{
     var newLine = [],i;
-    for (var i=0; i<newLine.length; i++); 
+    for (i=0; i<newLine.length; i++); 
     newLine.push('${i+1}.${newLine[i]}');
   }
   return 'The line is currently: ${newLine}'
