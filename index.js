2da779668224291cd0fc572f033ab01c42a70b25
Fei Qi <feiqi9047@gmail.com>
2018-11-15 18:58:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,19 +18,4 @@ function CurrenLine(numline){
   if (numline.length===0){
     return 'There is nobody waiting to be served!'
   } else{
-    var newLine = [],i;
-    for (i=0; i<numline.length;i++); 
-    newLine.push(' ${i+1}. ${numline[i]}');
-  }
-  return 'The line is currently: ${newLine}'
-}
-
-takeANumber(katzDeliLine, "Ada");
-takeANumber(katzDeliLine, "Grace"); 
-takeANumber(katzDeliLine, "Kent");
-currentLine(katzDeliLine);
-nowServing(katzDeliLine);
-currentLine(katzDeliLine);
-takeANumber(katzDeliLine, "Matz");
-curnowServing(katzDeliLine); 
-currentLine(katzDeliLine); 
+ 
